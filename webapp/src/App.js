c9fe40d86eb188a2fb477bfc74f6c2b738c18ed8
andres2906@gmail.com <andres2906@gmail.com>
2020-03-20 17:04:25 -0500

    configure material-ui to project

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import { Route, NavLink } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
 
 import Home from './pages/Home';
 import SomePage from './pages/SomePage';
 
+let theme = createMuiTheme({
+  typography: {
+    h5: {
+      fontWeight: 500,
+      fontSize: 26,
+      letterSpacing: 0.5
+    }
+  },
+  palette: {
+    primary: {
+      light: '#63ccff',
+      main: '#009be5',
+      dark: '#006db3'
+    }
+  }
+});
+
 function App() {
-  return(
-    <main>
-      <NavLink to='/'>Home </NavLink> &nbsp;&nbsp;
-      <NavLink to='/some-page'>Some page </NavLink>
-      <hr/>
-      <Route exact path='/' render={() => <Home />} />
-      <Route path='/some-page' render={() => <SomePage />} />
-    </main>
+  return (
+    <ThemeProvider theme={theme}>
+      <main>
+        <NavLink to='/'>Home </NavLink> &nbsp;&nbsp;
+        <NavLink to='/some-page'>Some page </NavLink>
+        <hr />
+        <Route exact path='/' render={() => <Home />} />
+        <Route path='/some-page' render={() => <SomePage />} />
+      </main>
+    </ThemeProvider>
   );
 }
 
