1d0e215a42c2fc91d7af91bbcf420c835817fb89
andres2906@gmail.com <andres2906@gmail.com>
2020-03-20 23:27:25 -0500

    add bottomTabs for home screen

--- webapp/src/pages/Home.js
+++ webapp/src/pages/Home.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import { AttachFile, CameraAlt, RestoreIcon } from '@material-ui/icons';
 
 function Home() {
+  const [currentTab, setCurrentTab] = useState(0);
+
   return (
-    <div>
-      <h1>Welcome to Cobic-19 AI Diagnosis</h1>
-      <a href="https://github.com/elcronos/COVID-19">https://github.com/elcronos/COVID-19</a>
+    <div className="ai-home">
+      <div className="ai-home__content">
+
+      </div>
+      <BottomNavigation
+        value={currentTab}
+        onChange={(event, newValue) => {
+          setCurrentTab(newValue);
+        }}
+        showLabels
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Camera" icon={<CameraAlt />} />
+        <BottomNavigationAction label="Attach" icon={<AttachFile />} />
+      </BottomNavigation>
     </div>
   );
 }
