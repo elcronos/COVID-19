0c2d5977e451645addd072f71172f5d0f20b2535
andres2906@gmail.com <andres2906@gmail.com>
2020-03-21 15:02:39 -0500

    configure react-intl in the project.

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,17 +4,31 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import 'typeface-roboto';
 
+
 import './scss/index.scss';
 import * as serviceWorker from './serviceWorker';
 
 import App from './App';
 import store from './store';
 
+import { IntlProvider } from 'react-intl';
+
+import messages_en from './translations/en.json';
+import messages_es from './translations/es.json';
+
+const messages = {
+  'en': messages_en,
+  'es': messages_es,
+};
+
+const language = navigator.language.split(/[-_]/)[0];
 const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <IntlProvider locale={language} messages={messages[language]}>
+        <App />
+      </IntlProvider>
     </Router>
   </Provider>,
   rootElement
