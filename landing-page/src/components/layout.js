74fde72b78866644112a56df5dd79d6215ea7f92
Juan Pablo Calao Perez <jcalaop@gmail.com>
2020-03-21 21:01:47 -0500

    Starter code for landing page

---
+++ landing-page/src/components/layout.js
@@ -0,0 +1,47 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+import "./layout.css"
+
+import { GenericContainer } from "../styles/generic.styles"
+
+const Layout = ({ children }) => {
+    const data = useStaticQuery(graphql`
+        query SiteTitleQuery {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `)
+
+    return (
+        <>
+            <Header siteTitle={data.site.siteMetadata.title} />
+            <GenericContainer>
+                <main>{children}</main>
+                <footer>
+                    © {new Date().getFullYear()}, Built with
+                    {` `}
+                    <a href="https://www.gatsbyjs.org">Gatsby</a>
+                </footer>
+            </GenericContainer>
+        </>
+    )
+}
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ landing-page/src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+    <Layout>
+        <SEO title="Home" />
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+    </Layout>
+)
+
+export default IndexPage
