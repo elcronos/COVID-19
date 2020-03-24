fdc59c6dfcf874de6671e57f9f8f4418fa24232c
Francisco Hanna <franciscohanna92@gmail.com>
2020-03-24 17:09:40 -0300

    Remove webapp project

--- webapp/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers';
-import thunk from 'redux-thunk';
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
